39a439d84a4bbf4a730b583433fbce18c4159ae0
zawar <zawar.ahmed74@myhunter.cuny.edu>
2020-04-13 10:46:37 -0400

    notes + labs

--- Mod_1/JSON_APIs/keys.py
+++ Mod_1/JSON_APIs/keys.py
@@ -1,2 +1,2 @@
-client_id = 'bVX1Jsfp4dkIOqw5HOVplg' #Your client ID goes here (as a string)
-api_key = 'RTzp-q-TgkJW_NFQogubFvZNRDziXyoR38VbtZMWibDI-FlvB25OE7GmafFEqhTL8_Bk2HlcX24-hRWLMP7Nc6WHO_VXMXldpPBjP0LoPv5EFFELMSI2oll8njhbXHYx' #Your api key goes here (as a string)
+client_id = 'iiD4cWOR-Fz3fX4le1knTg' #Your client ID goes here (as a string)
+api_key = 'gVojruYHjAZGYpsmLozBDSpP7qswB8zmqcy3mYwvXEIMJEIcx1HMil3rIC297GhpfgYqi2qZfXzVju4h_gcZfFGxLXUNTIXxRlxtIYnvhbTDRAfJ80Oi-xiTDG2QXnYx' #Your api key goes here (as a string)
